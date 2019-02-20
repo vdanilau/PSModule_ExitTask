f836115a834bcf52b4654eb0f2880e6040180a01
vdanilau <47238767+vdanilau@users.noreply.github.com>
2019-02-21 02:12:33 +0300

    Add files via upload

---
+++ Task_4.ps1
@@ -0,0 +1,20 @@
+﻿#4.	Проверить на удалённых компьютерах состояние одной службы (имя определить самостоятельно). 
+#Перечень имен компьютеров должен браться из текстового файла. 
+#Вывод организовать следующим образом: Имя компьютера – статус (если служба запущена, то строка зелёная, иначе красная)
+$cred=Get-Credential administrator
+$VM=Get-Content  F:\VM.txt
+foreach ($compname in $VM) {
+$serv=Invoke-Command -ComputerName $compname -Credential $cred -ScriptBlock {Get-Service -Name Wecsvc} #после -Name указываем интересующую службу
+if ($serv.status -eq "running"){
+    Write-Host "$compname -" -ForegroundColor Green -NoNewline
+    Write-Host "$serv.name -" -ForegroundColor Green -NoNewline
+    Write-Host $serv.Status -ForegroundColor Green}
+else {
+    Write-Host "$compname -" -ForegroundColor Red -NoNewline
+    Write-Host "$serv.name -" -ForegroundColor Red -NoNewline
+    Write-Host $serv.Status -ForegroundColor Red
+    } 
+}
+
+#Wecsvc -служба будет остановлена
+#dhcp -служба будет работать
